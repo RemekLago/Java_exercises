8c2f4a19bd697dd2b20711f67799041fc0ad1172
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-13 12:33:36 +0200

    new file

---
+++ basics/Operators.java
@@ -0,0 +1,42 @@
+package basics;
+public class Operators {
+
+    public static void main(String[] args) {
+
+        /*
+        mathematical operators
+        + sum, addition
+        - substraction
+        * multiplication
+        / devision
+        
+        = value assigment
+        +=
+        -=
+        /=
+        =
+        %=
+        
+        % the rest of the division (modulo)
+        
+        ++ incrementation:  + 1
+        -- decrementation:  - 1
+        
+        y++ - POST INKREMENTATION
+        y-- - POST decrementation
+        
+        ++y - PRE INKREMENTATION
+        --y - PRE decrementation
+        
+         */
+        
+        int a = 5;
+        
+         // a += 1;
+        
+        System.out.println(a--);
+        System.out.println(a);
+        System.out.println(--a);
+    }
+    
+}
