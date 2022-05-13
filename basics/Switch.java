5cdd6229402fdc388d3d07207cdb9818a22f5ebc
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-13 13:36:08 +0200

    new file

---
+++ basics/Switch.java
@@ -0,0 +1,20 @@
+package basics;
+
+public class Switch {
+    public static void main(String[] args){
+        
+    int a = 50;
+    switch(a)
+    {
+        case 50:
+            System.out.println("a = 50");
+        case 100:
+            System.out.println("a = 100");
+        case 200:
+            System.out.println("a = 200");
+        default:
+            System.out.println("a = " + a);
+
+    }
+    }
+}
