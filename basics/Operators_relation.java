fe7de33ef8f03ee0628d6695563a4b752dbd793d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-13 12:34:53 +0200

    new file

---
+++ basics/Operators_relation.java
@@ -0,0 +1,24 @@
+package basics;
+
+public class Operators_relation {
+
+    public static void main(String[] args) {
+
+        /*
+            operators
+            == 
+            !=
+            >
+            < 
+            >=
+            <=
+            
+
+        */
+
+        boolean a = 4 == 4;
+        if (4 == 4)
+            System.out.println(a);
+    }
+    
+}
