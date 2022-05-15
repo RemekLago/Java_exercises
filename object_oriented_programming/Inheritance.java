1536d18e6fbfbd4456679940bbd51dfb85f4a71d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-15 13:05:09 +0200

    update

--- object_oriented_programming/Inheritance.java
+++ object_oriented_programming/Inheritance.java
@@ -3,15 +3,21 @@ package object_oriented_programming;
 import object_oriented_programming.Monsters_inheritance.Monster;
 import object_oriented_programming.Monsters_inheritance.Skeleton;
 import object_oriented_programming.Monsters_inheritance.Zombie;
+import object_oriented_programming.Monsters_inheritance.Spider;
 
 public class Inheritance {
     public static void main(String[] args) {
 
-        Monster m = new Monster();
+        // Monster m = new Monster();
         Skeleton s = new Skeleton();
         Zombie z = new Zombie();
+        Spider sp = new Spider();
 
-        System.out.println(m.speed);
+        Monster s2 = new Skeleton();
+        Monster z2 = new Zombie();
+        Monster sp2 = new Spider();
+
+        System.out.println(sp.speed);
         System.out.println(s.speed);
         System.out.println(z.speed);
     }
