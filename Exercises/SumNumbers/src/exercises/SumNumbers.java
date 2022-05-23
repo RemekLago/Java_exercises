61ee995f3aab1003a26f3556333583ae62c06c71
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-23 12:20:20 +0200

    new task

---
+++ Exercises/SumNumbers/src/exercises/SumNumbers.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class SumNumbers {
+    public static void main(String[] args) {
+
+    int x, y, z;
+
+    x = 5;
+    y = 10;
+    z = x + y;
+
+    System.out.println
+            ("Sum: " + z);
+
+    System.out.println
+            ("Sum: " + (double)z);
+
+    }
+}
