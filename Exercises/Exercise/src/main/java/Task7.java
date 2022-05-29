a9c6c28be0fd6f778dc271d6b2b2365de741e79b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-29 09:07:17 +0200

    new task

---
+++ Exercises/Exercise/src/main/java/Task7.java
@@ -0,0 +1,15 @@
+public class Task7 {
+    public static void main(String[] args) {
+
+        int number = 10;
+
+        switch (number % 2) {
+            case 0:
+                System.out.println("Number is even");
+                break;
+            case 1:
+                System.out.println("Number is odd");
+                break;
+        }
+    }
+}
