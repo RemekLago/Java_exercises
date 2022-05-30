ce14ff66dcab7a9140e72b654f39978f566a4418
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-30 17:19:01 +0200

    new task

---
+++ Exercises/Exercise/src/main/java/Animals/AnimalPosition.java
@@ -0,0 +1,18 @@
+package Animals;
+
+public class AnimalPosition {
+    private double x, y;
+
+    public AnimalPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
