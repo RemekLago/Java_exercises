92cc5373fb0497b89672f8860f9f49752f9b2f9b
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-05-30 17:19:20 +0200

    new task

---
+++ Exercises/Exercise/src/main/java/Animals/AnimalTerritory.java
@@ -0,0 +1,18 @@
+package Animals;
+
+public class AnimalTerritory {
+    private double x, y;
+
+    public AnimalTerritory(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
