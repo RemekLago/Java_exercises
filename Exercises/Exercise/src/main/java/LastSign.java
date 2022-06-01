4e5d980cdd67c756c1d3efcad3029db6c27c1ba6
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-06-01 21:41:46 +0200

    new task

---
+++ Exercises/Exercise/src/main/java/LastSign.java
@@ -0,0 +1,11 @@
+//public class LastSign {
+//    public static void main(String[] args) {
+//
+//        System.out.println(printLastSign("Witaj!"));
+//        System.out.println(printLastSign("Ala ma kota"));
+//        System.out.println(printLastSign("?"));
+//    }
+//    public static char printLastSign(String s) {
+//        return s.charAt(s.length() - 1);
+//    }
+//}
